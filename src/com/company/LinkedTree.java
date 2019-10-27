fd1fc77741f2456cb29e606e9c2eaeb10536b0ec
Franicrack <56386158+Franicrack@users.noreply.github.com>
2019-10-27 23:56:53 +0100

    Update LinkedTree.java

--- src/com/company/LinkedTree.java
+++ src/com/company/LinkedTree.java
@@ -1,168 +1,202 @@
 package com.company;
 
-import com.sun.source.tree.Tree;
-import com.sun.source.tree.TreeVisitor;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class LinkedTree <E> implements NAryTree<E> {
+public class LinkedTree<E> implements NAryTree<E> {
+    private class TreeNode<T> implements Position<T> {
+        private T element; // The element stored in the position
+        private TreeNode<T> parent; // The parent of the node
+        private List<TreeNode<T>> children; // The children of the node
+        private LinkedTree<T> myTree; // A reference to the tree where the node belongs
 
-
-    private class LinkedTreeNode<E>implements Position<E>{
-        private E element;
-        private LinkedTreeNode<E>parent;
-        private List<LinkedTreeNode<E>> children;
-        private LinkedTree<E> myTree;
-
-        public void setElement(E element) {
-            this.element = element;
+        public TreeNode(LinkedTree<T> t, T e, TreeNode<T> p, List<TreeNode<T>> c) {
+            this.element = e;
+            this.parent = p;
+            this.children = c;
+            this.myTree = t;
         }
-
-        public E getElement() {
+        public T getElement() {
             return element;
         }
 
-        public void setChildren(List<LinkedTreeNode<E>> children) {
-            this.children = children;
+        public final void setElement(T o) {
+            element = o;
         }
 
-        public void setMyTree(LinkedTree<E> myTree) {
-            this.myTree = myTree;
-        }
-
-        public void setParent(LinkedTreeNode<E> parent) {
-            this.parent = parent;
-        }
-
-        public LinkedTreeNode<E> getParent(){
-            return this.parent;
-        }
-        public List<LinkedTreeNode<E>> getChildren() {
+        public List<TreeNode<T>> getChildren() {
             return children;
         }
 
-        public LinkedTree<E> getMyTree() {
+        public final void setChildren(List<TreeNode<T>> c) {
+            children = c;
+        }
+
+        public TreeNode<T> getParent() {
+            return parent;
+        }
+
+        public final void setParent(TreeNode<T> v) {
+            parent = v;
+        }
+
+        public LinkedTree<T> getMyTree() {
             return myTree;
         }
 
-        public LinkedTreeNode(Position<E>p, E element, LinkedTree<E>myTree, List<LinkedTreeNode<E>>children){
-            this.parent=(LinkedTreeNode)p;
-            this.element=element;
-            this.myTree=myTree;
-            this.children=children;
+        public void setMyTree(LinkedTree<T> myTree) {
+            this.myTree = myTree;
         }
     }
-    private LinkedTreeNode<E> root;
-    private int size;
-    public LinkedTree(){
-        this.size=0;
-        this.root=null;
+
+    private TreeNode<E> root; // The root of the tree
+    private int size; // The number of nodes in the tree
+
+    public LinkedTree() {
+        root = null;
+        size = 0;
     }
 
-    public int getSize() {
+    public int size() {
         return size;
     }
 
-    public LinkedTreeNode<E> getRoot() {
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public boolean isInternal(Position<E> v) {
+        return !isLeaf(v);
+    }
+
+    public boolean isLeaf(Position<E> p) {
+        TreeNode<E> node = checkPosition(p);
+        return (node.getChildren() == null) || (node.getChildren().isEmpty());
+    }
+
+    public boolean isRoot(Position<E> p) {
+        TreeNode<E> node = checkPosition(p);
+        return (node == this.root());
+    }
+
+    public Position<E> root() throws RuntimeException {
+        if (root == null) {
+            throw new RuntimeException("The tree is empty");
+        }
         return root;
     }
 
-    public void setRoot(LinkedTreeNode<E> root) {
-        this.root = root;
+    public Position<E> parent(Position<E> p) throws RuntimeException {
+        TreeNode<E> node = checkPosition(p);
+        Position<E> parentPos = node.getParent();
+        if (parentPos == null) {
+            throw new RuntimeException("The node has not parent");
+        }
+        return parentPos;
     }
 
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public boolean isEmpty (){
-        return this.size==0;
-    }
-
-    public boolean isInternal(Position<E>p){
-        return !isLeaf(p);
-    }
-
-    public boolean isLeaf(Position<E>p){
-        LinkedTreeNode<E>node=checkPosition(p);
-        return (node.getChildren()==null)||(node.getChildren().isEmpty());
-    }
-
-    public boolean isRoot(Position<E>p){
-        LinkedTreeNode<E>node=checkPosition(p);
-        return (node==this.root);
-    }
-
-    public Position<E> root() throws RuntimeException{
-        if(root==null)
-            throw new RuntimeException("The tree is Empty");
-        return root;
-    }
-
-    public Position<E> parent(Position<E>p) throws RuntimeException{
-        LinkedTreeNode<E>node=checkPosition(p);
-        Position<E>parent=node.getParent();
-        if (parent==null)
-            throw new RuntimeException("No tiene padres");
-        return parent;
-    }
-
-    public Iterable<? extends Position<E>> children (Position<E>p){
-        LinkedTreeNode<E>node=checkPosition(p);
+    public Iterable<? extends Position<E>> children(Position<E> p) {
+        TreeNode<E> node = checkPosition(p);
         return node.getChildren();
     }
 
-    public E replace(Position<E>p,E elem){
-        LinkedTreeNode<E>node=checkPosition(p);
-        E temp=p.getElement;
-        node.setElement(elem);
+    public E replace(Position<E> p, E e) {
+        TreeNode<E> node = checkPosition(p);
+        E temp = p.getElement();
+        node.setElement(e);
         return temp;
     }
 
-    public Position<E> addRoot (E elem) throws RuntimeException{
-        if (!isEmpty())
-            throw new RuntimeException("The tree has already a root");
-        size=1;
-        root= new LinkedTreeNode<E>(null,elem,this, new ArrayList<>());
+    public Position<E> addRoot(E e) throws RuntimeException {
+        if (!isEmpty()) {
+            throw new IllegalStateException("Tree already has a root");
+        }
+        size = 1;
+        root = new TreeNode<E>(this, e, null, new ArrayList<>());
         return root;
     }
 
-    public void swapElements(Position<E>p1,Position<E>p2){
-        LinkedTreeNode<E> node1 = checkPosition(p1);
-        LinkedTreeNode<E> node2 = checkPosition(p2);
-        E temp = p2.getElement;
-        node2.setElement(p1.getElement);
+
+
+    public void swapElements(Position<E> p1, Position<E> p2) {
+        TreeNode<E> node1 = checkPosition(p1);
+        TreeNode<E> node2 = checkPosition(p2);
+        E temp = p2.getElement();
+        node2.setElement(p1.getElement());
         node1.setElement(temp);
     }
 
-    private LinkedTreeNode<E> checkPosition(Position<E> p) throws RuntimeException {
-        if (p == null || !(p instanceof LinkedTreeNode)) {
-            throw new RuntimeException("The position is invalid");
+    private TreeNode<E> checkPosition(Position<E> p) throws IllegalStateException {
+        if (p == null || !(p instanceof TreeNode)) {
+            throw new IllegalStateException("The position is invalid");
         }
-        LinkedTreeNode<E> aux = (LinkedTreeNode<E>) p;
+        TreeNode<E> aux = (TreeNode<E>) p;
         if (aux.getMyTree() != this) {
-            throw new RuntimeException("The node is not from this tree");
+            throw new IllegalStateException("The node is not from this tree");
         }
         return aux;
     }
 
     public Position<E> add(E element, Position<E> p) {
-        LinkedTreeNode<E> parent = checkPosition(p);
-        LinkedTreeNode<E> newNode = new LinkedTreeNode<E>(parent,element,this,new ArrayList<>());
-        List<LinkedTreeNode<E>> l = parent.getChildren();
+        TreeNode<E> parent = checkPosition(p);
+        TreeNode<E> newNode = new TreeNode<E>(this, element, parent, new ArrayList<>());
+        List<TreeNode<E>> l = parent.getChildren();
         l.add(newNode);
         size++;
         return newNode;
     }
 
+    public void remove(Position<E> p) {
+        TreeNode<E> node = checkPosition(p);
+        if (node.getParent() != null) {
+            Iterator<Position<E>> it = new BFSIterator<E>(this, p);
+            int cont = 0;
+            while (it.hasNext()) {
+                TreeNode<E> next = checkPosition(it.next());
+                next.setMyTree(null);
+                cont++;
+            }
+            size = size - cont;
+            TreeNode<E> parent = node.getParent();
+            parent.getChildren().remove(node);
+        } else {
+            this.root = null;
+            this.size = 0;
+        }
+        node.setMyTree(null);
+    }
+
     public Iterator<Position<E>> iterator() {
-        Iterator<Position<E>> it = new BFSIterator<>(this);
-        return it;
+        return new BFSIterator<>(this);
     }
 
     public void moveSubtree(Position<E> pOrig, Position<E> pDest) throws RuntimeException {
-
+        TreeNode<E>node= checkPosition(pOrig);
+        List<Position<E>> aux= new ArrayList<>();
+        TreeNode<E> nodeDest= checkPosition(pDest);
+        if (node==this.root)
+            throw new RuntimeException("Cant move the root");
+        if (pOrig==pDest)
+            throw new RuntimeException("pOrig = pDest");
+        BFSIterator<E> it= new BFSIterator(this,pOrig);
+        while(it.hasNext()){
+            aux.add(it.next());
+        }
+        int i;
+        boolean equals;
+        i=0;
+        equals= false;
+        while ((i<=aux.size())&& (equals==false)){
+            TreeNode<E>aux2= checkPosition(aux.get(i));
+            if (nodeDest==aux2)
+                equals=true;
+        }
+        if (equals==true)
+            throw new RuntimeException("Dest node is inside of pOrig subtree");
+        TreeNode<E> parent= node.getParent();
+        node.setParent(nodeDest);
+        nodeDest.children.add(node);
+        parent.children.remove(node);
     }
 }
